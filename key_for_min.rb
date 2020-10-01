2d35b82608e27994fa4403de650d8f6985f8da03
Vivek Sanghvi <viksipart@gmail.com>
2020-10-01 16:43:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,6 +9,18 @@
     #smallest_val = v 
    # smallest_key = k 
 #  end
-end
-smallest_key
+#end
+#smallest_key
+#end
+
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
 end
