d28690ab8269f36a86472510fb782fa33389167e
Vivek Sanghvi <viksipart@gmail.com>
2020-10-01 17:11:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil 
   lowest_value = nil 
 name_hash.each do |k, v|
-  if v < lowest_value || lowest_value == nil 
+  if lowest_value == nil || v < lowest_value  
     lowest_value = v 
     lowest_key = k 
   end
